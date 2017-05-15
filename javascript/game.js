0054e0d4dd2bff8a82d820d2958c3858fc4d1b89
Contrapuntist <contrapuntist@gmail.com>
2017-05-14 23:01:48 -0500

    added some css

--- javascript/game.js
+++ javascript/game.js
@@ -2,15 +2,14 @@
 //variable declarations 
 var userChoice = ""; 
 var wordSelect = ""; 
-
+var charMatch = null;
 //collection of words to use in game 
 var wordLibrary = "guitar"; 
 
-var gotWords = ['targaryen', 'throne', 'westeros', 'blackfyre', 'stark', 'reyne', 'dragon', 'dragonloards'];
+var gotWords = ['targaryen', 'throne', 'westeros', 'blackfyre', 'stark', 'reyne', 'dragon', 'dragonlords'];
 
 //random selection of word.  
 var wordChoice = gotWords[Math.floor(Math.random() * gotWords.length)];  
-
 
 
 // Loop for determining length of selected word and how many underscores to print to screen 
@@ -23,7 +22,21 @@ console.log(wordChoice);
 
 document.querySelector('#wordSelect').innerHTML = wordSelect; 
 
-//getElementById('')
+document.onkeyup = function(event) {
+	userChoice = event.key; 
 
-//.("#wordSelect").innerHTML = wordSelect;  querySelector 
+	for (var i = 0; i < wordChoice.length; i++ ) {
+	
+	if (userChoice == wordChoice.charAt(i) ) {
+		charMatch = true;
+		
+	} else {
+		charMatch = false;
+	}
+
+	console.log(charMatch);
+	}
+
+	document.querySelector('#userChoice').innerHTML = userChoice; 
+}
 
