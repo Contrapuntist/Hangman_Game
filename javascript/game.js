32838d48e05923104ed28dec695a8a2698867678
Contrapuntist <contrapuntist@gmail.com>
2017-05-17 22:44:16 -0500

    game edits and background image added

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,5 @@
 
-//variable declarations 
+// Variable declarations 
 var userChoice = ""; 
 var wordSelect = []; 
 var charMatch = null;
@@ -8,42 +8,41 @@ var fullAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 var hangman = 7;
 
 
-//collection of words to use in game 
-
-//var wordLibrary = "guitar"; 
+// Collection of words to use in game 
 var gotWords = ['targaryen', 'throne', 'westeros', 'blackfyre', 'stark', 'reyne', 'dragon', 'dragonlords'];
 
-//random selection of word.  
+// Random selection of word.  
 var wordChoice = gotWords[Math.floor(Math.random() * gotWords.length)];  
-
 
 // Loop for determining length of selected word and how many underscores to print to screen 
 for ( var i = 0; i < wordChoice.length; i++ ) { 
+	
 	wordSelect[i] = " _";
-	console.log(wordSelect);
 	document.querySelector('#wordSelect').innerHTML = wordSelect.join(" ");
 } 	
 
+// Know what word is selected from array gotWords to help with debugging   
 console.log(wordChoice);
 
-//document.querySelector('#wordSelect').innerHTML = wordSelect; 
+// Document.querySelector('#wordSelect').innerHTML = wordSelect; 
 document.querySelector('#movesLeft').innerHTML = hangman; 
 
-//Taking in user input on key press and printing results of choice to HTML 
+// Taking in user input on key press and printing results of choice to HTML 
 document.onkeyup = function(event) {
+
 	userChoice = event.key; 
-	charUsed.push(" " + userChoice);
-	letterMatch(userChoice);
-	gameCounter(charMatch);
 
-	
-//	console.log(hangman); 
-//	console.log(charUsed);
-	
-	document.querySelector('#userChoice').innerHTML = userChoice; 
-	document.querySelector('#alphaUsed').innerHTML = charUsed;
-	document.querySelector('#movesLeft').innerHTML = hangman;
+	if ( fullAlphabet.indexOf(userChoice) !== -1  ) { 
 
+		charUsed.push(" " + userChoice);
+		letterMatch(userChoice);
+		gameCounter(charMatch);
+		document.querySelector('#userChoice').innerHTML = userChoice; 
+		document.querySelector('#alphaUsed').innerHTML = charUsed;
+		document.querySelector('#movesLeft').innerHTML = hangman;
+
+	} 
+	
 }
 
 //function that checks whether user choice is in the word
@@ -60,7 +59,7 @@ function letterMatch(letter) {
 	}
 	
 	document.querySelector('#wordSelect').innerHTML = wordSelect.join(' ');
-	console.log(charMatch); 
+
 }
 
 function gameCounter () {
@@ -69,4 +68,4 @@ function gameCounter () {
 		hangman = hangman - 1;
 	}
 
-}
+} 
